e72b7f1c940b70f1aff1c06f20fa577aa7a033e5
Trystan Binkley-Jones <tcbinkleyj@gmail.com>
2012-10-30 22:24:06 -0600

    Added more stuff

---
+++ src/core/Mass.java
@@ -0,0 +1,15 @@
+package core;
+
+/**
+ * Provides a basic mass that can move through space under the influence of gravity.
+ * 
+ * @author Trystan Binkley-Jones
+ *
+ */
+public class Mass
+{
+	/** 
+	 * The mass of the object.
+	 */
+	double mass;
+}
---
+++ src/core/TestPuzzle.java
@@ -0,0 +1,34 @@
+package core;
+
+public final class TestPuzzle extends Puzzle
+{
+
+	@Override
+	public void InitializePuzzle()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void Tick(float dt)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Boolean HasWon()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double GetParameter(String parameter)
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
