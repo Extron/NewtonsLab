991a74edbc5fbd1c5d40b6b8d2648aaef49a417e
Trystan Binkley-Jones <tcbinkleyj@gmail.com>
2012-11-07 21:58:55 -0700

    Now we have a puzzle that runs.

--- src/applet/NLRootPanel.java
+++ src/applet/NLRootPanel.java
@@ -1,9 +1,15 @@
 package applet;
 
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+
+import core.TestPuzzle;
 
 /**
  * The main panel component that holds all of the GUI components for the applet.
@@ -11,17 +17,39 @@ import javax.swing.JPanel;
  * @author Trystan Binkley-Jones
  *
  */
-public class NLRootPanel extends JPanel
+public class NLRootPanel extends JPanel implements ActionListener
 {
+	/**
+	 * The puzzle component that is being used for the applet.
+	 */
+	PuzzleComponent puzzle;
+	
 	/**
 	 * This is the default constructor for the panel, and it is here where the GUI will be created.
 	 */
 	public NLRootPanel()
 	{
+		setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+		
 		//Note that a JLabel can have an icon attached to it.  We may want one.
 		JLabel title = new JLabel("Newton's Lab");
 		title.setFont(new Font("Arial", Font.BOLD, 36));
 		
+		puzzle = new PuzzleComponent(new TestPuzzle());
+		
+		JButton activate = new JButton("Activate puzzle.");
+		activate.setActionCommand("activate");
+		activate.addActionListener(this);
+		
 		add(title);
+		add(puzzle);
+		add(activate);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e)
+	{
+		if (e.getActionCommand() == "activate")
+			puzzle.ActivatePuzzle();
 	}
 }
--- src/applet/PuzzleComponent.java
+++ src/applet/PuzzleComponent.java
@@ -1,13 +1,17 @@
 package applet;
 
+import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import java.util.Iterator;
 
 import javax.swing.JComponent;
+import javax.swing.event.MouseInputListener;
 
 import core.Puzzle;
 import core.PuzzleElement;
+import core.Vector2;
 
 /**
  * This is where we will display a puzzle.  By extending the <code>JComponent</code> class, we can
@@ -16,7 +20,7 @@ import core.PuzzleElement;
  * @author Trystan Binkley-Jones
  *
  */
-public class PuzzleComponent extends JComponent
+public class PuzzleComponent extends JComponent implements MouseInputListener
 {
 	/**
 	 * A list of the objects in the puzzle to draw.
@@ -24,9 +28,29 @@ public class PuzzleComponent extends JComponent
 	ArrayList<PuzzleObject> objects;
 	
 	/**
+	 * This is the location of the center of the viewport of this component.
+	 */
+	Vector2 viewport;
+	
+	/**
 	 * The current puzzle that is being drawn.
 	 */
 	Puzzle puzzle;
+	
+	/**
+	 * The scale of the viewport.
+	 */
+	double scale;
+	
+	/**
+	 * The delta time step to use for puzzle ticking.
+	 */
+	double dt;
+	
+	/**
+	 * The old mouse coordinates.
+	 */
+	int mX, mY;
 	
 	
 	/**
@@ -38,9 +62,17 @@ public class PuzzleComponent extends JComponent
 	{
 		this.puzzle = puzzle;
 		
+		setSize(500, 500);
+		viewport = new Vector2(250, 250);
+		scale = 1;
+		dt = 1.0 / 16.0;
+		
 		objects = new ArrayList<PuzzleObject>();
 		
 		InitElements();
+		
+		addMouseListener(this);
+		addMouseMotionListener(this);
 	}
 	
 	
@@ -51,12 +83,97 @@ public class PuzzleComponent extends JComponent
 	public void paintComponent(Graphics g)
 	{
 		super.paintComponent(g);
+	
+		//Fill the background.  TODO: Make this customizable to draw things like pictures.
+		int x = getInsets().left;
+		int y = getInsets().top;
+		int w = getWidth() - getInsets().left - getInsets().right;
+		int h = getHeight() - getInsets().top - getInsets().bottom;
+		
+		g.setColor(Color.white);
+		
+		g.fillRect(x, y, w, h);
 		
 		//Draw all of the puzzle objects.
 		Iterator<PuzzleObject> iter = objects.iterator();
 		
 		while (iter.hasNext())
-			iter.next().Draw(g);
+			iter.next().Draw(g, viewport, scale);
+	}
+	
+	@Override
+	public void mouseMoved(MouseEvent e) 
+	{
+		mX = e.getX();
+		mY = e.getY();
+		
+		//System.out.println("Mouse Pos:" + mX + " " + mY);
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e)
+	{
+		// TODO Auto-generated method stub
+		double dx = e.getX() - mX;
+		double dy = e.getY() - mY;
+		
+		viewport.x += dx;
+		viewport.y += dy;
+		
+		mX = e.getX();
+		mY = e.getY();
+		
+		repaint();
+	}
+	
+	@Override
+	public void mouseClicked(MouseEvent e)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void mouseEntered(MouseEvent e)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void mouseExited(MouseEvent e)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void mousePressed(MouseEvent e)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void mouseReleased(MouseEvent e)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	/**
+	 * Gets the puzzle that the puzzle component is drawing.
+	 * 
+	 * @return Returns the puzzle component's current puzzle.
+	 */
+	public Puzzle GetPuzzle()
+	{
+		return puzzle;
 	}
 	
 	/**
@@ -64,7 +181,21 @@ public class PuzzleComponent extends JComponent
 	 */
 	public void ActivatePuzzle()
 	{
-		puzzle.ActivatePuzzle();
+		if (puzzle.CanActivate())
+		{
+			puzzle.ActivatePuzzle();
+		
+			PuzzleTickWorker tick = new PuzzleTickWorker(this);
+			tick.execute();
+		}
+	}
+	
+	/**
+	 * Ticks the puzzle one frame.
+	 */
+	public void TickPuzzle()
+	{
+		puzzle.Tick(dt);
 	}
 	
 	/**
@@ -72,6 +203,8 @@ public class PuzzleComponent extends JComponent
 	 */
 	private void InitElements()
 	{
+		puzzle.InitializePuzzle();
+		
 		ArrayList<PuzzleElement> elements = puzzle.GetElements();
 		
 		Iterator<PuzzleElement> iter = elements.iterator();
